ba62c60bfdedfc118fa715b981479aafab80d68b
OZANE Corentin <corentin.ozane@enseirb-matmeca.fr>
2024-03-12 10:07:10 +0100

    Ajout constructeur SharedFile

--- src/peer/src/SharedFile.java
+++ src/peer/src/SharedFile.java
@@ -1,49 +1,58 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SharedFile {
 
     // Nom du fichier
     private String filename;
 
+    // The shared file
+    private File file;
+
     // Clé hexadécimale du fichier
     private String key;
 
     // Taille en octets du fichier
-    private int size;
+    private long size;
 
     // Taille en octets d'une pièce du fichier
     private int pieceSize;
     
-    public SharedFile(String filename) {
-        this.filename = filename;
+    /**
+     * Constructor of the SharedFile class
+     * 
+     * @param path The path to the file
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     */
+    public SharedFile(String path) throws IOException, NoSuchAlgorithmException {
+        this.file = new File(path);
+        this.filename = this.file.getName();
         this.key = computeKey();
-
+        this.size = Files.size(this.file.toPath());
+        pieceSize = 1024;
     }
 
-    private String computeKey() {
+    /**
+     * Takes the path of the file and computes the hexadecimal key of the file.
+     * 
+     * @return The hexadecimal key of the file
+     * @throws IOException
+     * @throws NoSuchAlgorithmException 
+     */
+    private String computeKey() throws IOException, NoSuchAlgorithmException {
 
-        String command = "md5sum " + this.filename;
+        byte[] data = Files.readAllBytes(Paths.get(this.file.toPath().toString()));
+        byte[] hash = MessageDigest.getInstance("MD5").digest(data);
+        String checksum = new BigInteger(1, hash).toString(16);
 
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.command("bash", "-c", command);
-        Process p;
-        try {
-            p = pb.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-
-        // Lecture de la sortie de la commande
-        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        StringBuilder output = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            output.append(line).append("\n");
-        }
-        
-        return "";
+        return checksum;
     }
 }
