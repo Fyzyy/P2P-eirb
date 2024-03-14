c4f3d1fefe7eee8e737e68cca87cec19dcdc0b77
mvidiani <mvidiani@enseirb.fr>
2024-03-14 19:23:12 +0100

    add \n to parsing test

--- tst/tracker/parser_test.c
+++ tst/tracker/parser_test.c
@@ -13,19 +13,19 @@
 #define RED_TEXT(text) "\033[0;31m" text "\033[0m"
 
 void test_parsing() {
-    char buffer1[] = "announce listen 2222 seed [file1 100 10 1234567890 file2 200 20 0987654321] leech [1221 212121]";
+    char buffer1[] = "announce listen 2222 seed [file1 100 10 1234567890 file2 200 20 0987654321] leech [1221 212121]\n";
     enum tokens result1 = parsing(buffer1, "0.0.0.0", 9999);
     assert(result1 == OK);
 
-    char buffer2[] = "look [filename=\"foo\" filesize>\"100\"]";
+    char buffer2[] = "look [filename=\"foo\" filesize>\"100\"]\n";
     enum tokens result2 = parsing(buffer2, "0.0.0.0", 9999);
     assert(result2 == LIST);
 
-    char buffer3[] = "getfile file_key";
+    char buffer3[] = "getfile file_key\n";
     enum tokens result3 = parsing(buffer3, "0.0.0.0", 9999);
     assert(result3 == PEERS);
 
-    char buffer4[] = "update seed [1 2 3] leech [4 5 6]";
+    char buffer4[] = "update seed [1 2 3] leech [4 5 6]\n";
     enum tokens result4 = parsing(buffer4, "0.0.0.0", 9999);
     assert(result4 == OK);
 
@@ -44,7 +44,7 @@ void test_limit_cases() {
     assert(result3 == UNKNOWN);
 
     // Test buffer with invalid syntax
-    char buffer4[] = "announce listen 2222 seed [file1 100 10 1234567890 file2 200 20 0987654321";
+    char buffer4[] = "announce listen 2222 seed [file1 100 10 1234567890 file2 200 20 0987654321] leech []\r";
     enum tokens result4 = parsing(buffer4, "0.0.0.0", 9999);
     assert(result4 == OK);
 }
