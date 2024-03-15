0edbe2470cf6e02bac0af9f062d83a79bba8a164
mvidiani <mvidiani@enseirb.fr>
2024-03-15 13:21:38 +0100

    exit command

--- src/tracker/tracker.c
+++ src/tracker/tracker.c
@@ -3,10 +3,23 @@
 #include "parser.h"
 
 #define MAX_PEERS_CONNECTIONS 10
+char input[100];
+int server_socket;
+
+void quit() {
+
+    remove_all_peers();
+    remove_list(connectedPeers);
+    remove_list(allPeers);
+    printf("exit tracker\n");
+    close(server_socket);
+    exit(EXIT_SUCCESS);
+}
 
 void handle_peer_connection(int socket, const char *ip, int port) {
     char buffer[MAX_BUFFER_SIZE];
     ssize_t bytes_received;
+    
     PeerInfo* peer = new_peer(connectedPeers, ip, port);
 
     while ((bytes_received = recv(socket, buffer, sizeof(buffer), 0)) > 0) {
@@ -35,27 +48,26 @@ void accept_connections(int server_socket) {
     struct sockaddr_in client_address;
     socklen_t client_address_len = sizeof(client_address);
 
-    #pragma omp parallel num_threads(MAX_PEERS_CONNECTIONS)
-    {
         int client_socket;
         char client_ip[INET_ADDRSTRLEN];
 
         while (1) {
-            #pragma omp critical
-            {
-                if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len)) == -1) {
-                    perror("Erreur lors de l'acceptation de la connexion");
-                    exit(EXIT_FAILURE);
-                }
-                inet_ntop(AF_INET, &(client_address.sin_addr), client_ip, INET_ADDRSTRLEN);
-                printf("Connexion acceptée de %s:%d\n", client_ip, ntohs(client_address.sin_port));
-                printf("connected peer : %d\n", connectedPeers->n_peers + 1);
-            }
 
-            handle_peer_connection(client_socket, client_ip, client_address.sin_port);
-            close(client_socket);
+            if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len)) == -1) {
+                perror("Erreur lors de l'acceptation de la connexion");
+                exit(EXIT_FAILURE);
+            }
+            inet_ntop(AF_INET, &(client_address.sin_addr), client_ip, INET_ADDRSTRLEN);
+            printf("Connexion acceptée de %s:%d\n", client_ip, ntohs(client_address.sin_port));
+            printf("connected peer : %d\n", connectedPeers->n_peers + 1);
+
+            #pragma omp task firstprivate(client_socket, client_ip, client_address) shared(connectedPeers, allPeers, server_socket)
+            {
+                handle_peer_connection(client_socket, client_ip, client_address.sin_port);
+                close(client_socket);
+            }
         }
-    }
+
 }
 
 int main() {
@@ -64,8 +76,6 @@ int main() {
     load_config("config.ini", &serverConfig);
 
     init_global_lists();
-
-    int server_socket;
     struct sockaddr_in server_address;
 
     // Créer une socket
@@ -93,31 +103,25 @@ int main() {
     printf("Serveur en attente de connexions sur le port %d...\n", serverConfig.port);
     puts("Tapez 'exit' pour quitter le tracker :");
 
-    char input[100];
-
     #pragma omp parallel
     {
         #pragma omp single
         {
             // Boucle principale
-            while (strncmp(input, "exit", 4) != 0) {
-                #pragma omp task shared(input)
-                fgets(input, sizeof(input), stdin);
+            while (1) {
 
-                // Accepter les connexions
-                #pragma omp task shared(server_socket) shared(connectedPeers) shared(allPeers)
+                #pragma omp task shared(input)
+                {
+                    fgets(input, sizeof(input), stdin);
+                    if (strncmp(input, "exit", 4) == 0)
+                        quit();
+                }
+
                 accept_connections(server_socket);
             }
         }
     }
-    
-    // Après la sortie de la boucle
-    puts("exit tracker\n");
-    remove_list(connectedPeers);
-    remove_list(allPeers);
-    remove_all_peers();
-
-    close(server_socket);
+    quit();
 
     return 0;
 }
