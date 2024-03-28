2fbb93eb38a7aaac8ad8043436a144f56eaf55c0
mvidiani <mvidiani@enseirb.fr>
2024-03-28 19:15:10 +0100

    thread pool

--- src/tracker/tracker.c
+++ src/tracker/tracker.c
@@ -10,22 +10,26 @@
 #include "config.h"
 #include "parser.h"
 
-#define MAX_PEERS_CONNECTIONS 10
-#define NUM_THREADS 5 // Nombre de threads dans le pool
+#define MAX_PEERS_CONNECTIONS 20
 
 char input[100];
 int server_socket;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+pthread_t stdin_thread;
+pthread_t thread_pool[MAX_PEERS_CONNECTIONS];
 
 void quit() {
-    // Mettre en place vos opérations de nettoyage ici
     reset_tracked_files();
     remove_list(connectedPeers);
     remove_list(allPeers);
     printf("Sortie du tracker\n");
     close(server_socket);
     exit(EXIT_SUCCESS);
+}
+
+void signal_to_quit() {
+
 }
 
 void handle_data(int socket, const char *ip, int port) {
@@ -35,16 +39,14 @@ void handle_data(int socket, const char *ip, int port) {
     PeerInfo* peer = new_peer(connectedPeers, ip, port);
 
     while ((bytes_received = recv(socket, buffer, sizeof(buffer), 0)) > 0) {
-        // Traiter les données reçues
+
         buffer[bytes_received] = '\0';
         printf("Données reçues de %s:%d : %s\n", ip, port, buffer);
         
         response* res = create_response(peer);
         parsing(buffer, res);
         send(socket, res->message, strlen(res->message), 0);
-        //TODO envoyer la réponse
         free(res);
-        sched_yield();
     }
 
     if (bytes_received == 0) {
@@ -54,12 +56,9 @@ void handle_data(int socket, const char *ip, int port) {
     } else if (bytes_received == -1) {
         perror("Erreur lors de la réception de données");
     }
-
-    pthread_exit(NULL);
-
 }
 
-void *handle_client(void *unused) {
+void *handle_client() {
     while (1) {
         int client_socket;
         struct sockaddr_in client_address;
@@ -83,20 +82,18 @@ void *handle_client(void *unused) {
         handle_data(client_socket, client_ip, (int) ntohs(client_address.sin_port));
         close(client_socket);
     }
-    return NULL;
 }
 
-void *handle_stdin(void *unused) {
+void *handle_stdin() {
     while (1) {
         fgets(input, sizeof(input), stdin);
         if (strncmp(input, "exit", 4) == 0)
-            quit();
+            pthread_exit(NULL);
         if (strncmp(input, "files", 5) == 0)
             display_tracked_files();
         if (strncmp(input, "peers", 5) == 0)
             display_connected_peer_info();
     }
-    return NULL;
 }
 
 int main() {
@@ -106,18 +103,15 @@ int main() {
     init_global_lists();
     struct sockaddr_in server_address;
 
-    // Créer une socket
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Erreur lors de la création de la socket");
         exit(EXIT_FAILURE);
     }
 
-    // Configurer l'adresse du serveur
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = inet_addr(serverConfig.address);
     server_address.sin_port = htons(serverConfig.port);
 
-    // Lier la socket à l'adresse et au port
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
         perror("Erreur lors de la liaison de la socket");
         exit(EXIT_FAILURE);
@@ -133,16 +127,11 @@ int main() {
     puts("'files' pour afficher la liste des fichiers suivis :");
     puts("'exit' pour quitter le tracker :");
 
-    pthread_t stdin_thread;
-    pthread_t thread_pool[NUM_THREADS];
-
-
-    pthread_create(&stdin_thread, NULL, handle_stdin, NULL);
-
-
-    for (int i = 0; i < NUM_THREADS; ++i) {
+    for (int i = 0; i < MAX_PEERS_CONNECTIONS; ++i) {
         pthread_create(&thread_pool[i], NULL, handle_client, NULL);
     }
+
+    pthread_create(&stdin_thread, NULL, handle_stdin, NULL);
 
     pthread_join(stdin_thread, NULL);
 
