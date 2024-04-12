49b8184d739b4d467bb9ddbd55dc3cb18a1ce8ad
Robin ROUET <rrouet@enseirb-matmeca.fr>
2024-04-12 09:27:15 +0200

    Add ethods to list bitmaps of the peer

--- peer/src/Main.java
+++ peer/src/Main.java
@@ -20,7 +20,8 @@ public class Main {
     private static final String DISCONNECT_COMMAND = "disconnect";
     private static final String SEND_COMMAND = "send";
     private static final String ADD_FILE_COMMAND = "add";
-    private static final String LIST_FILES_COMMAND = "list";
+    private static final String REMOVE_FILE_COMMAND = "remove";
+    private static final String LIST_COMMAND = "list";
 
     private static ConfigReader config = new ConfigReader();
 
@@ -164,7 +165,9 @@ public class Main {
         System.out.println("To send message to peer, type: " + SEND_COMMAND + " \"$message\" $ip1:$port1 $ip2:$port2 ...\n");
         System.out.println("To exit the client, type: " + EXIT_COMMAND + "\n");
         System.out.println("To add a file to the peer storage, type: " + ADD_FILE_COMMAND +  " file $path_to_file\n");
-        System.out.println("To list files in peer storage, type: list files");
+        System.out.println("To remove a file to the peer storage, type: " + REMOVE_FILE_COMMAND +  " file $path_to_file\n");
+        System.out.println("To list files in peer storage, type: list files\n");
+        System.out.println("To list bitmap in peer storage, type: list bitmap");
         System.out.println("**************************************************************");
         }
     
@@ -188,7 +191,7 @@ public class Main {
                     break;
 
                 case ADD_FILE_COMMAND:
-                    if (tokens.length != 3){
+                    if (tokens.length < 3){
                         usage();
                     }
 
@@ -197,11 +200,26 @@ public class Main {
                     }
                     break;
 
-                case LIST_FILES_COMMAND:
+                case REMOVE_FILE_COMMAND:
+                    if (tokens.length < 3){
+                        usage();
+                    }
+                    
+                    else if (tokens[1].equals("file")) {
+                        peer.removeFile(tokens[2]);
+                    }
+                    break;
+                
+                case LIST_COMMAND:
                     if (tokens[1].equals("files")){
                         peer.listFiles();
                     }
-                    else{
+                    
+                    else if (tokens[1].equals("bitmap")){
+                        peer.listBitMap();
+                    }
+
+                    else {
                         usage();
                     }
                     break;
--- peer/src/Peer.java
+++ peer/src/Peer.java
@@ -98,6 +98,11 @@ public class Peer {
         fileManager.listFiles();
     }
 
+    public void listBitMap() {
+        System.out.println("Bit map of the peer: ");
+        fileManager.getBitMap();
+    }
+
     public void addFile(String filePath) {
         try {
             System.out.println("Adding file " + filePath + " to peer storage...");
@@ -105,6 +110,16 @@ public class Peer {
             System.out.println("Done");
         } catch (Exception e) {
             System.out.println("Cannot add file to peer storage");
+        }
+    }
+
+    public void removeFile(String filePath) {
+        try {
+            System.out.println("Removing " + filePath + " to peer stockage...");
+            fileManager.removeFile(filePath);
+            System.out.println("Done");
+        } catch (Exception e) {
+            System.out.println("Cannot remove the file");
         }
     }
 
